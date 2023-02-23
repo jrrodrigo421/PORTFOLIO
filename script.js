540f7499ca56df0eb2b76d7ecba5ff4a843a8a7f
Rodrigo Lopes <rjr89000@gmail.com>
2023-02-23 13:09:25 -0300

    Adcionando Responsividade

---
+++ script.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+  $(window).scroll(function(){
+    if(this.scrollY > 20){
+      $('.navbar').addClass("sticky");
+    }else{
+      $('.navbar').removeClass("sticky");
+
+    };
+  });
+});
+
+
+
+// function start(){
+//   alert('teste')
+// }
+// start()
