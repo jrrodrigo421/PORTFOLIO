3a6ad093d08483c99d9d436390952954aaf66cb5
Rodrigo Lopes <rjr89000@gmail.com>
2023-03-01 08:17:05 -0300

    ajustes...

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 ## Site Responsivo [ HTML, CSS, JavaScript (Jquery) ]
 
 
-Portfólio ainda em desenvolvimento [ loading ]
+Portfólio ainda em desenvolvimento [ loading ... ]
 Mas já disponível para acompanhamento em tempo real para mudanças futuras que serão realizadas:
 
 https://rodrigolopes.netlify.app
--- script.js
+++ script.js
@@ -18,13 +18,13 @@ $(document).ready(function(){
    var typed = new Typed(".typing", {
        strings:[" à procura de uma oportunidade como Desenvolvedor JR."],
        typeSpeed:50,
-       backSpeed:60,
+       backSpeed:80,
        loop:true
   });
    var typed = new Typed(".typing-2", {
-      strings:["Estudande de Programação", "Formado em Gestão de Telecomunicações pelo INATEL","Apaixonado por tecnologia"],
-      typeSpeed:100,
-      backSpeed:60,
+      strings:["Estudande de Programação,", "Formado em Gestão de Telecomunicações pelo INATEL,","em busca do aperfeiçoamento profissional,"],
+      typeSpeed:20,
+      backSpeed:80,
       loop:true
   });
 
@@ -54,11 +54,6 @@ $(document).ready(function(){
   // });
   
 });
-
-// $('.menu-btn').click(function(){
-//   $('.navbar .menu').toggleClass("active");
-//   $('.menu-btn i').toggleClass("active");
-// });
 
 document.getElementById("contratar").addEventListener("click", function() {
   window.location.href = "https://api.whatsapp.com/send?phone=5535999539535";
