38b168c03684bbe11f986e07cc2835a0c1b82774
Rodrigo Lopes <rjr89000@gmail.com>
2023-03-01 08:20:11 -0300

    ajustes...

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ $(document).ready(function(){
        loop:true
   });
    var typed = new Typed(".typing-2", {
-      strings:["Estudande de Programação,", "Formado em Gestão de Telecomunicações pelo INATEL,","em busca do aperfeiçoamento profissional,"],
+      strings:["Estudande de Programação,", "Formado em Gestão de Telecomunicações pelo INATEL,","de Santa Rita do Sapucaí - MG,"],
       typeSpeed:20,
       backSpeed:80,
       loop:true
